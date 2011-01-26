cc5312c27fddeb478b4979722db3fab5a3f7246c
James Halliday <substack@gmail.com>
2011-01-26 22:41:47 +0000

    untested code with a package.json

---
+++ index.js
@@ -0,0 +1,52 @@
+module.exports = function (res) {
+    var _writeHead = res.writeHead;
+    var _end = res.end;
+    var headers = {};
+    var status = null;
+    var calledWriteHead = false;
+    
+    res.setHeader = function (key, value) {
+        headers[key] = value;
+        return res;
+    };
+    
+    res.setHeaders = function (hs) {
+        Object.keys(hs).forEach(function (key) {
+            headers[key] = hs[key];
+        });
+        return res;
+    };
+    
+    res.type = function (t) {
+        return res.setHeader('Content-Type', t);
+    };
+    
+    res.status = function (s) {
+        status = s;
+    };
+    
+    res.end = function (msg) {
+        if (!calledWriteHead) {
+            res.writeHead(status || 200, {});
+        }
+        _end.call(res, msg);
+        return res;
+    };
+    
+    res.writeHead = function (s, hs) {
+        if (s) {}
+        status = s;
+        
+        if (headers) {
+            Object.keys(hs).forEach(function (key) {
+                headers[key] = hs[key];
+            }); 
+        }
+        calledWriteHead = true;
+        
+        _writeHead.call(res, status, headers);
+        return res;
+    };
+    
+    return res;
+};
